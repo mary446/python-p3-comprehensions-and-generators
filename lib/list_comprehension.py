7d9c869003d8fbd425719b8e90b0e826eb7fe0ef
mary446 <135317707+mary446@users.noreply.github.com>
2023-08-18 12:40:38 +0300

    passed all

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,18 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
+    num=[]
+    for i in num_list:
+        if i%2==0:
+            num.append(i)
+    return num
     pass
 
 def make_exclamation(sentence_list):
-    pass
+    sent=[]
+    if len(sentence_list)>0:
+        for sentence in sentence_list:
+            sent.append(sentence[:]+"!")
+        
+    
+    return sent
